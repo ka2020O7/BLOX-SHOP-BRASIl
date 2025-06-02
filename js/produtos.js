12dc7b99884b6c13d5f97d92431b2ef160b5eaac
Ka2020O7 <kauan_fernandes3@estudante.sesisenai.org.br>
2025-06-01 22:05:18 -0300

    alinhei as imagens e os nomes dekas

---
+++ js/produtos.js
@@ -0,0 +1,72 @@
+const produtos = {
+  1: {
+    titulo: "RAÇA V4 ANGEL FULL GEAR",
+    preco: "29,90",
+    precoAntigo: "34,90",
+    descricao: "Conta com Raça V4 Angel + Full Gear + Bônus",
+    imagem: "../css/assets/angel.png"
+  },
+  2: {
+    titulo: "RAÇA V4 GHOUL FULL GEAR",
+    preco: "29,90",
+    precoAntigo: "34,90",
+    descricao: "Conta com Raça V4 Ghoul + Full Gear + Bônus",
+    imagem: "../css/assets/ghoul.png"
+  },
+  3: {
+    titulo: "RAÇA V4 CYBORG FULL GEAR",
+    preco: "29,90",
+    precoAntigo: "34,90",
+    descricao: "Conta com Raça V4 Cyborg + Full Gear + Bônus",
+    imagem: "../css/assets/cyborg.png"
+  },
+  4: {
+    titulo: "LEVEL 2550 GODHUMAN DRAGON",
+    preco: "39,90",
+    precoAntigo: "44,90",
+    descricao: "Conta Level 2550 com Godhuman + Dragon + Bônus",
+    imagem: "../css/assets/01JAR39JBNYW829P49TJMGGAZS-offers.png"
+  },
+  5: {
+    titulo: "LEVEL 2600 GAS GODHUMAN",
+    preco: "44,90",
+    precoAntigo: "49,90",
+    descricao: "Conta Level 2600 com Gas + Godhuman + Bônus",
+    imagem: "../css/assets/01JFPC0C4MCQTT2XX3C9C5PBMN-offers.png"
+  },
+  6: {
+    titulo: "LEVEL 2600 SANGUINE ART",
+    preco: "44,90",
+    precoAntigo: "49,90",
+    descricao: "Conta Level 2600 com Sanguine Art + Bônus",
+    imagem: "../css/assets/01J8FX98S3WBQR2B8VQNC3GB66-offers.png"
+  },
+  7: {
+    titulo: "LEVEL 2600 T-REX GODHUMAN",
+    preco: "44,90",
+    precoAntigo: "49,90",
+    descricao: "Conta Level 2600 com T-Rex + Godhuman + Bônus",
+    imagem: "../css/assets/01J8FZSKFC59AMHN7K2S4KQJZH-offers.png"
+  },
+  8: {
+    titulo: "LEVEL 2600 DOUGH GODHUMAN",
+    preco: "44,90",
+    precoAntigo: "49,90",
+    descricao: "Conta Level 2600 com Dough + Godhuman + Bônus",
+    imagem: "../css/assets/01J8FZV8ACNT5S3CE6A4H902AC-offers.png"
+  },
+  9: {
+    titulo: "LEVEL 2600 LEOPARD GODHUMAN",
+    preco: "44,90",
+    precoAntigo: "49,90",
+    descricao: "Conta Level 2600 com Leopard + Godhuman + Bônus",
+    imagem: "../css/assets/01J8FZWQZRSRKWN3V459TVJW4H-offers.png"
+  },
+  10: {
+    titulo: "LEVEL 2600 KITSUNE GODHUMAN",
+    preco: "44,90",
+    precoAntigo: "49,90",
+    descricao: "Conta Level 2600 com Kitsune + Godhuman + Bônus",
+    imagem: "../css/assets/01J8FZXX26W1MV1RC0RAZZRKVD-offers.png"
+  }
+};
